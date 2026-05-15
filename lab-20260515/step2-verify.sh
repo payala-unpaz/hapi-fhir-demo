c566ac5cddb4b289711390f041e87808c2868138
payala-unpaz <pablo.ayala@docentes.unpaz.edu.ar>
2026-05-15 06:58:02 -0300

    Delete lab-20260515/step2-verify.sh

--- lab-20260515/step2-verify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Asumiendo que el estudiante debe nombrar al contenedor 'hapi-fhir'
-if [ "$(docker ps -a -q -f name=hapi-fhir)" ]; then
-  exit 0
-else
-  echo "No se encontró un contenedor llamado 'hapi-fhir'. ¿Ejecutaste el comando docker run?"
-  exit 1
-fi
